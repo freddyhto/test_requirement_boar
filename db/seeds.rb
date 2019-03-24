d3e9fa089dde79e5595c55467357582b0ddce173
Freddy Tellez <fre.tellez@gmail.com>
2019-03-23 23:04:33 -0300

    seeds to create dummy users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,9 @@
+#seeds to issues
 Issue.create!(title: "Bug on positive vote button", description: "the positive vote button has a double call")
 Issue.create!(title: "Bug on view of votes", description: "The count of votes don't update on the view")
 Issue.create!(title: "Problem with loggin", description: "The users cant loggin with his google users")
 Issue.create!(title: "Too long request", description: "Calls to API take more than 1 minute")
+
+#seed to users
+User.create!(email: "fre.tellez@gmail.com", name: "Freddy Tellez")
+User.create!(email: "freddyhto@hotmail.com", name: "Pedro Perez")
